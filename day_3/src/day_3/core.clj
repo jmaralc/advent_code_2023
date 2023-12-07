c310d2b29f3b274b97ee761597940255c71abb7c
jmaralc <jmaralc@gmail.com>
2023-12-07 23:47:03 +0100

    feat: part 1 day 3 solved

--- day_3/src/day_3/core.clj
+++ day_3/src/day_3/core.clj
@@ -1,7 +1,7 @@
 (ns day-3.core
   (:gen-class))
 
-(def special-characters ["#" "+" "$" "*"])
+(def special-characters ["#" "+" "$" "*" "/" "%" "=" "&" "-" "@"])
 (def numbers ["0" "1" "2" "3" "4" "5" "6" "7" "8" "9"])
 
 (defn index-engine-lines
@@ -58,15 +58,23 @@
   (and (= (get point-1 0) (get point-2 0)) (<= (abs (- (get point-1 1) (get point-2 1))) 1))
   )
 
-(defn have-neighbours
-  [collection next-position]
-  (if (= (count collection) 0) [next-position]
-                               (cond
-                                 (neighbours-positions? (last collection) next-position) collection
-                                 :else (concat collection [next-position])
-                                 )
-                               )
+(defn remove-neighbours
+  [original-collection new-collection current]
+  (if (> current (- (count original-collection) 1))
+    (sort new-collection)
+    (if (= current 0)
+      (recur  original-collection (conj new-collection (first original-collection)) (inc current))
+      (let [current-position (nth original-collection current) ]
+        (if (neighbours-positions? current-position (nth original-collection (- current 1)))
+          (recur  original-collection new-collection (inc current))
+          (recur  original-collection (conj new-collection current-position)  (inc current))
+          )
+        )
+      )
     )
+  )
+
+
 (defn get-neighbours-positions
   [engine-characters engine-numbers]
   (into [] (reduce (fn[col cur] (concat col cur)) (map (fn[character] (get-number-neighbours character engine-numbers)) engine-characters)
@@ -77,7 +85,7 @@
   (let [engine-characters (detect-engine-characters input)]
     (let [engine-numbers (detect-engine-numbers input)]
       (let [neighbour-positions (get-neighbours-positions engine-characters engine-numbers)]
-        (reduce have-neighbours [] (sort neighbour-positions))
+        (remove-neighbours (sort neighbour-positions) [] 0)
         )
       )))
 
@@ -87,13 +95,21 @@
   )
 
 (defn my-left-search
-  [y current-line numbers-to-the-left]
-  (if (< y 0) numbers-to-the-left
-              (when (>= y 0)
-              (let [current-char (get current-line y)]
-                (if (not (some #(= (str current-char) %) numbers))
-                  numbers-to-the-left
-              (recur (dec y) current-line (str current-char numbers-to-the-left ))))))
+  [index current-line numbers-to-the-left]
+  (if (< index 0) numbers-to-the-left
+  (let [current-char (get current-line index)]
+    (if (not (some #(= (str current-char) %) numbers))
+      numbers-to-the-left
+  (recur (dec index) current-line (str current-char numbers-to-the-left )))))
+  )
+
+(defn my-right-search
+  [index current-line numbers-to-the-right]
+  (if (> index (count current-line)) numbers-to-the-right
+                  (let [current-char (get current-line index)]
+                    (if (not (some #(= (str current-char) %) numbers))
+                      numbers-to-the-right
+                      (recur (inc index) current-line (str numbers-to-the-right current-char)))))
   )
 
 (defn get-whole-numbers
@@ -101,7 +117,9 @@
   (let [initial-number (get-number-of-candidate input-lines candidate)]
     (let [current-line (get input-lines (get candidate 0))]
       (let [left-numbers (my-left-search (- (get candidate 1) 1) current-line "")]
-        [(Integer/parseInt (str left-numbers initial-number))]
+        (let [right-numbers (my-right-search  (+ (get candidate 1) 1) current-line "")]
+          (Long/parseLong (str left-numbers initial-number right-numbers))
+          )
         )
       )
 
@@ -117,9 +135,20 @@
 
 (defn get-part-numbers
   [input]
-  (get-candidate-numbers input)
+  (let [input-lines (clojure.string/split-lines input)]
+    (map (fn[candidate] (get-whole-numbers input-lines candidate)) (get-candidate-positions input))
+    )
   )
 
+(defn sum-part-numbers
+  [input]
+  (reduce + (get-part-numbers input))
+  )
+
+(defn get-sum-of-part-numbers-of-file
+  [file-path]
+  (sum-part-numbers (slurp file-path))
+  )
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
