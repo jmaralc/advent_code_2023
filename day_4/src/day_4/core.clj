44e9d1a568fee232cd9b6712efd9134234dffb80
Javier Martinez Alcantara <jmaralc@gmail.com>
2023-12-10 02:09:45 +0100

    feat: part 2 of day 4 done

--- day_4/src/day_4/core.clj
+++ day_4/src/day_4/core.clj
@@ -13,6 +13,11 @@
   [string]
   (Integer/parseInt string)
   )
+(defn get-card-index
+  [card]
+  (let [card-head (first (clojure.string/split card #":")) ]
+    (Integer/parseInt (clojure.string/replace (last (clojure.string/split card-head #"[ \t]+")) #":" ""))
+    ))
 
 (defn get-numbers
   [card]
@@ -34,6 +39,45 @@
     )
   )
 
+(defn map-to-n
+  [n]
+  (zipmap (range 1 n) (repeat 1))
+  )
+
+(defn get-initial-carry
+  [input]
+  (zipmap (range 1 (+ (count input) 1)) (repeat 1))
+  )
+
+(defn update-carry-on-index
+  [carry index original-card-index]
+  (update carry index + (* 1 (get carry original-card-index)))
+  )
+(defn update-carry
+  [index wins carry]
+  (let [range-of-cards (range (+ index 1) (+ index wins 1))]
+    (reduce (fn [col val] (update-carry-on-index col val index))  carry range-of-cards)
+    )
+  )
+(defn get-card-carry
+  [carry card]
+  (let [index (get-card-index card)]
+    (let [deck-size (last (sort (keys carry)) )]
+      (if (> index deck-size) carry
+                              (let [[wining-numbers my-numbers]  (get-numbers card) ]
+                                (let [common-numbers (clojure.set/intersection wining-numbers my-numbers)]
+                                  (let [wins (count common-numbers)]
+                                    (if (> wins 0) (update-carry index wins carry) carry)
+                                    )
+                                  )
+                                )
+                              )
+      )
+    )
+  )
+
+
+
 (defn split-game-in-cards
   [input]
   (clojure.string/split-lines input)
@@ -44,10 +88,23 @@
   (reduce + (map get-card-points (split-game-in-cards input)) )
   )
 
+(defn compute-all-cards
+  [input]
+  (let [cards (split-game-in-cards input)]
+    (reduce + (vals (reduce get-card-carry (get-initial-carry cards) cards)) )
+    )
+  )
+
 (defn get-points-of-input-file
   [file-path]
   (compute-total-points (slurp file-path))
   )
+
+(defn get-cards-of-input-file
+  [file-path]
+  (compute-all-cards (slurp file-path))
+  )
+
 
 (defn -main
   "I don't do a whole lot ... yet."
--- day_4/test/day_4/core_test.clj
+++ day_4/test/day_4/core_test.clj
@@ -29,6 +29,13 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")
             "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
             ]))))
 
+(deftest get-card-index-test
+  (testing "should return the index of the card"
+    (is (= (get-card-index "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11") 6))
+    (is (= (get-card-index "Card  45: 17 36 61 18 49 99 48 13 90 46 | 98 47 19 29 76 71 96 33 59 55 43 38 73  7 66 58 28 23 60  8 39 65 95 86 81") 45))
+    ))
+
+
 (deftest get-number-of-a-card-test
   (testing "should return two values, one for the wining numbers one for y numbers"
     (is (= (get-numbers "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53") [(set [41 48 83 86 17])  (set [83 86  6 31 17  9 48 53]) ]))))
@@ -57,3 +64,31 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")
 (deftest get-points-from-a-card-test6
   (testing "should return 0 points for the sixth card"
     (is (= (get-card-points "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11") 0))))
+
+
+(deftest get-initial-carry-test
+  (testing "should return a carry element with 1 for all the card numbers")
+  (is (= (get-initial-carry ["Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+                             "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"
+                             "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"
+                             "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"
+                             "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"
+                             "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+                             ]) {1 1, 2 1, 3 1, 4 1, 5 1, 6 1}))
+  )
+(deftest get-points-from-a-card-test
+  (testing "should return one copy each of the next four cards: cards 2, 3, 4, and 5. for the first card"
+    (is (= (get-card-carry
+             {1 1, 2 1, 3 1, 4 1, 5 1, 6 1}
+             "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+             )
+           {1 1, 2 2, 3 2, 4 2, 5 2, 6 1}))))
+
+(deftest compute-all-the-final-cards-for-test-input
+  (testing "should return the final amount of cards for the test input"
+    (is (= (compute-all-cards test-input) 30)))
+  )
+
+(deftest test-total-cards
+  (testing "should return the amount of cards"
+    (is (= (get-cards-of-input-file "./resources/input.txt") 5329815))))
