60b1c930d82b9c9c0b36c1c91ae48d859d83ad7f
Javier Martinez Alcantara <jmaralc@gmail.com>
2023-12-11 22:51:07 +0100

    feat: part 1 day 6 solved

---
+++ day_6/src/day_6/core.clj
@@ -0,0 +1,42 @@
+(ns day-6.core
+  (:gen-class))
+
+
+(defn combine-press-time-and-max-distance
+  [press-times max-distances]
+  (map (fn [index] [(nth press-times index)  (nth max-distances index)]) (range (count press-times)))
+  )
+
+(defn extract-press-time
+  [input]
+  (map (fn [x] (Integer/parseInt x)) (re-seq #"\d+" input))
+  )
+
+(defn extract-max-dist
+  [input]
+  (map (fn [x] (Integer/parseInt x)) (re-seq #"\d+" input))
+  )
+
+
+(defn split-input
+  [input]
+  (clojure.string/split-lines input)
+  )
+
+(defn get-distance
+  [press-time total-time]
+  (* (- total-time press-time) press-time)
+  )
+(defn compute-ways-to-beat-record
+  [[press-time max-distance]]
+  (count (filter (fn [x] (> x max-distance)) (map (fn [pt] (get-distance pt press-time)) (range press-time))))
+  )
+
+(defn compute-multiply-of-races-posibilities
+  [input]
+  (let [[press-time-input max-distance-input] (split-input input)]
+    (let [games (combine-press-time-and-max-distance (extract-press-time press-time-input) (extract-max-dist max-distance-input))]
+      (reduce * (map compute-ways-to-beat-record games))
+      )
+    )
+  )
---
+++ day_6/test/day_6/core_test.clj
@@ -0,0 +1,46 @@
+(ns day-6.core-test
+  (:require [clojure.test :refer :all]
+            [day-6.core :refer :all]))
+
+(def test-input "Time:      7  15   30
+Distance:  9  40  200")
+
+(deftest split-input-test
+  (testing "should split the input in time and distance components"
+    (is (= (split-input test-input)) ["Time:      7  15   30" "Distance:  9  40  200"])))
+
+(deftest extract-press-time-test
+  (testing "should return an array of press times"
+    (is (= (extract-press-time "Time:      7  15   30") [7 15 30]))))
+
+(deftest extract-max-dist-test
+  (testing "should return an array of press times"
+    (is (= (extract-max-dist "Distance:  9  40  200") [9 40 200]))))
+
+(deftest combine-press-time-and-max-distance-test
+  (testing "should return an array with pairs of press time and max distance"
+    (is (= (combine-press-time-and-max-distance [7 15 30] [9 40 200])))
+    ))
+
+(deftest compute-ways-to-beat-record-test
+  (testing "should return 4 for the first race of test input"
+    (is (= (compute-ways-to-beat-record [7 9] ) 4))
+    ))
+(deftest compute-ways-to-beat-record-test
+  (testing "should return 8 for the first race of test input"
+    (is (= (compute-ways-to-beat-record [15 40] ) 8))
+    ))
+(deftest compute-ways-to-beat-record-test
+  (testing "should return 9 for the first race of test input"
+    (is (= (compute-ways-to-beat-record [30 200] ) 9))
+    ))
+
+(deftest compute-multiply-of-races-posibilities-test-input
+  (testing "should return 288for for the test input"
+    (is (= (compute-multiply-of-races-posibilities test-input ) 288))
+    ))
+
+(deftest compute-multiply-of-races-posibilities-test-input
+  (testing "should return 288for for the test input"
+    (is (= (compute-multiply-of-races-posibilities (slurp "./resources/input.txt") ) 288))
+    ))
